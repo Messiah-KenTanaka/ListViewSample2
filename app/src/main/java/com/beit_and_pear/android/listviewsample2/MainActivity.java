8739d86569ad7492397d772be3842345f8fe6ff7
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2021-12-28 20:49:16 +0900

    ダイアログを設定

--- app/src/main/java/com/beit_and_pear/android/listviewsample2/MainActivity.java
+++ app/src/main/java/com/beit_and_pear/android/listviewsample2/MainActivity.java
@@ -3,6 +3,13 @@ package com.beit_and_pear.android.listviewsample2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lvMenu = findViewById(R.id.lvMenu);
+        List<String> menuList = new ArrayList<>();
+
+        menuList.add("唐揚げ定食");
+        menuList.add("ハンバーグ定食");
+        menuList.add("生姜焼き定食");
+        menuList.add("ステーキ定食");
+        menuList.add("野菜炒め定食");
+        menuList.add("トンカツ定食");
+        menuList.add("ミンチカツ定食");
+        menuList.add("チキンカツ定食");
+        menuList.add("コロッケ定食");
+        menuList.add("回鍋肉定食");
+        menuList.add("青椒肉絲定食");
+        menuList.add("焼き魚定食");
+        menuList.add("焼肉定食");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, menuList);
+        lvMenu.setAdapter(adapter);
+
+        // リストビューにリスナを設定
+        lvMenu.setOnItemClickListener(new ListItemClickListener());
+    }
+
+    // リストがタップされた時の処理が記述されたメンバクラス
+    private class ListItemClickListener implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            // 注文確認ダイアログフラグメントオブジェクトを生成
+            OrderConfirmDialogFragment dialogFragment = new OrderConfirmDialogFragment();
+            // ダイアログを表示
+            dialogFragment.show(getSupportFragmentManager(), "OrderConfirmDialogFragment");
+        }
     }
 }
